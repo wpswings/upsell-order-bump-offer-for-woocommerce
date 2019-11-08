44c242dd097c682789352ac92d1aaaad63e4d6fb
ced-ashutoshv <ashutoshverma@cedcoss.com>
2019-11-08 14:08:28 +0530

    Version updated

--- upsell-order-bump-offer-for-woocommerce.php
+++ upsell-order-bump-offer-for-woocommerce.php
@@ -21,7 +21,7 @@
  * WC requires at least:    3.0
  * WC tested up to:         3.8.0
  *
- * Version:           1.0.2
+ * Version:           1.2.0
  * Author:            MakeWebBetter
  * Author URI:        https://makewebbetter.com/
  * License:           GPL-3.0
@@ -63,7 +63,7 @@ function mwb_ubo_lite_is_plugin_active( $plugin_slug = '' ) {
 /**
  * Currently plugin version.
  */
-define( 'UPSELL_ORDER_BUMP_OFFER_FOR_WOOCOMMERCE_VERSION', '1.0.2' );
+define( 'UPSELL_ORDER_BUMP_OFFER_FOR_WOOCOMMERCE_VERSION', '1.2.0' );
 
 /**
  * The code that runs during plugin activation.
