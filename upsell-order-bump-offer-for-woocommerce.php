2fd230337bf49107b3d012fded257eed771f4bb2
prince-cedcoss <princekumaryadav@makewebbetter.com>
2022-12-22 16:52:46 +0530

    qa_fixes

--- upsell-order-bump-offer-for-woocommerce.php
+++ upsell-order-bump-offer-for-woocommerce.php
@@ -19,7 +19,7 @@
  * Requires at least:       4.4
  * Tested up to:            6.1.1
  * WC requires at least:    3.0.0
- * WC tested up to:         7.2.0
+ * WC tested up to:         7.2.2
  *
  * Version:           2.1.8
  * Author:            WP Swings
