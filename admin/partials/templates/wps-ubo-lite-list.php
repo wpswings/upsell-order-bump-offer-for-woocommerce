fd5953d6974d6c757054cc70fe5f2e61da01fe6c
prince-cedcoss <princekumaryadav@makewebbetter.com>
2024-03-11 17:39:38 +0530

    button add

--- admin/partials/templates/wps-ubo-lite-list.php
+++ admin/partials/templates/wps-ubo-lite-list.php
@@ -214,8 +214,15 @@ $wps_upsell_bumps_list = get_option( 'wps_ubo_bump_list' );
 <?php endif; ?>
 
 <!-- Create New Bump. -->
+<?php $installed_plugins = get_plugins(); ?>
+<!-- Create New Bump. -->
 <div class="wps_upsell_bump_create_new_bump">
-	<a href="?page=upsell-order-bump-offer-for-woocommerce-setting&tab=creation-setting&bump_id=1" class="wps_ubo_lite_bump_create_button" ><?php esc_html_e( '+Create New Order Bump', 'upsell-order-bump-offer-for-woocommerce' ); ?></a>
+	<a href="?page=upsell-order-bump-offer-for-woocommerce-setting&tab=creation-setting&bump_id=<?php echo esc_html( $wps_upsell_bumps_last_index + 1 ); ?>"><?php esc_html_e( '+Create New Order Bump', 'upsell-order-bump-offer-for-woocommerce-pro' ); ?></a>
+	<?php if ( is_plugin_active( 'woo-one-click-upsell-funnel/woocommerce-one-click-upsell-funnel.php' ) ) { ?>
+	<a href="?page=wps-wocuf-setting&tab=funnels-list"><?php esc_html_e( '+Create New Post Order Bump', 'upsell-order-bump-offer-for-woocommerce-pro' ); ?></a>
+	<?php } elseif ( ! array_key_exists( 'woo-one-click-upsell-funnel/woocommerce-one-click-upsell-funnel.php', $installed_plugins ) ) { ?>
+	<a id = "wps_one_click_upsell" href="#"><?php esc_html_e( '+Create New Post Order Bump', 'upsell-order-bump-offer-for-woocommerce-pro' ); ?></a>
+	<?php } ?>
 </div>
 
 <!-- Add Go pro popup. -->
