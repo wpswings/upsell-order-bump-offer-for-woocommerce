c2015d5e28e05f9b98ceb8ee23831da61ae0eb4d
prince-cedcoss <princekumaryadav@makewebbetter.com>
2024-05-15 11:01:18 +0530

    fixes

--- public/js/wps_ubo_lite_public_script.js
+++ public/js/wps_ubo_lite_public_script.js
@@ -5,20 +5,22 @@ jQuery(document).ready(function ($) {
         $('.wrapup_order_bump').hide();
     }
 
-    var bodyElements = document.getElementsByTagName('body')[0].getElementsByTagName('*');
-    for (var i = 0; i < bodyElements.length; i++) {
-        if (bodyElements[i].classList.contains('wrapup_order_bump')) {
-            bodyElements[i].style.display = 'none';
-            console.log('First occurrence of the specific class found:', bodyElements[i]);
-            break;
-        }
-    }
-    
+
     setTimeout(function () {
 
         var wps_is_checkout_block_use = wps_ubo_lite_public.wps_is_checkout_block_use;
 
         if (wps_is_checkout_block_use) {
+
+            var bodyElements = document.getElementsByTagName('body')[0].getElementsByTagName('*');
+            for (var i = 0; i < bodyElements.length; i++) {
+                if (bodyElements[i].classList.contains('wrapup_order_bump')) {
+                    bodyElements[i].style.display = 'none';
+                    console.log('First occurrence of the specific class found:', bodyElements[i]);
+                    break;
+                }
+            }
+
             if (jQuery('.wrapup_order_bump').length > 0) {
                 var data = jQuery('.wrapup_order_bump').html();
 
