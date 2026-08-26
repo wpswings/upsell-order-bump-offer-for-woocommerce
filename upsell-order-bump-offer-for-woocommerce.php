66457e2c50ddb029c62168142c6a5534fb612d6c
ananya-cedcoss <ananyashukla@wpswings.com>
2026-08-26 14:15:48 +0530

    WPS-7709 Version updated

--- upsell-order-bump-offer-for-woocommerce.php
+++ upsell-order-bump-offer-for-woocommerce.php
@@ -22,7 +22,7 @@
  * WC tested up to:         10.8.1
  *
  * Requires Plugins: woocommerce
- * Version:           3.1.5
+ * Version:           3.1.6
  * Author:            WP Swings
  * Author URI:        https://wpswings.com/?utm_source=wpswings-official&utm_medium=order-bump-org-backend&utm_campaign=official
  * License:           GPL-3.0
@@ -145,7 +145,7 @@ if ( $wps_ubo_activated ) {
 	/**
 	 * Currently plugin version.
 	 */
-	define( 'UPSELL_ORDER_BUMP_OFFER_FOR_WOOCOMMERCE_VERSION', '3.1.5' );
+	define( 'UPSELL_ORDER_BUMP_OFFER_FOR_WOOCOMMERCE_VERSION', '3.1.6' );
 	if ( ! defined( 'WPS_WOCUF_URL_FUNNEL_BUILDER' ) ) {
 		define( 'WPS_WOCUF_URL_FUNNEL_BUILDER', plugin_dir_url( __FILE__ ) );
 	}
